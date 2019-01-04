a8893a2308ec6e0f0abc67d39196908fbc9f8d32
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2019-01-03 23:23:27 -0500

    hlsbucket.go: A few lines of web server code.

--- hlsbucket.go
+++ hlsbucket.go
@@ -16,6 +16,7 @@ import (
 	// "strconv"
 	"time"
 	"hash/crc32"
+	"net/http"
 )
 
 var CFGPATH = "hlsbucket.json"
@@ -79,6 +80,17 @@ func handlePacket(buffer []byte, saveDir string) {
 			log.Printf("handlePacket write error")
 		}
 	}
+}
+
+func http_server() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Either pick apart the url, or set up a few different
+		// handlers.
+		fmt.Fprintf(w, "hlsbucket request %s\n", r.URL.Path)
+	})
+
+	err := http.ListenAndServe(":8004", nil)
+	log.Printf(err.Error())
 }
 
 // I wrote this tiny function because I'm spoiled by other
@@ -194,6 +206,9 @@ func main() {
 		}
 	}()
 
+	go http_server()
+
+	// Main loop relays packets between goroutines.
 	var data []byte
 	var rconn net.Conn
 	var rconnSet bool = false
