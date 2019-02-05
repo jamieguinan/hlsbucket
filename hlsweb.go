f554b5e7d4028ce12f3c2988c16f1295e97835d6
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2019-02-05 08:16:39 -0500

    Basic HLS working.

--- hlsweb.go
+++ hlsweb.go
@@ -5,33 +5,40 @@ import (
 	"net/http"
 	"fmt"
 	"log"
-//	"net/url"
+	"os"
+	//"time"
+	"path/filepath"
 )
 
 
-func live_m3u8(w http.ResponseWriter, r *http.Request) {
-	//  "#EXTM3U\n"
-	//  "#EXT-X-TARGETDURATION:%d\n", priv->duration
-	//  "#EXT-X-VERSION:3\n"
-	//  "#EXT-X-MEDIA-SEQUENCE:%d\n", priv->media_sequence
-	//  "#EXTINF:%d,\n"
-	//  "file1.ts\n"
-	//  "#EXTINF:%d,\n"
-	//  "file2.ts\n"
-	//  "#EXTINF:%d,\n"
-	//  "file3.ts\n"
+func live_index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "audio/x-mpegurl")
+
+	duration := 3
+
+	fmt.Fprintf(w,"#EXTM3U\n")
+	fmt.Fprintf(w,"#EXT-X-TARGETDURATION:%d\n", duration)
+	fmt.Fprintf(w,"#EXT-X-VERSION:3\n")
+	fmt.Fprintf(w,"#EXT-X-MEDIA-SEQUENCE:%d\n", g.mediaSequence)
 
 	g.recentLock.Lock()
 	for e:=g.recent.Front(); e != nil; e = e.Next() {
 		// Return everything up to but not including the
 		// most recent file, which is still being written.
 		if e != g.recent.Back() {
+			fmt.Fprintf(w,"#EXTINF:%d, no desc\n", duration)
 			fmt.Fprintf(w, "%s\n", e.Value)
 		}
 	}
 
 	g.recentLock.Unlock()
+}
 
+func live_meta(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "audio/x-mpegurl")
+	fmt.Fprintf(w, "#EXTM3U\n")
+	fmt.Fprintf(w, "#EXT-X-STREAM-INF:PROGRAM-ID=1, BANDWIDTH=100000\n")
+	fmt.Fprintf(w, "live_index.m3u8\n")
 }
 
 
@@ -43,15 +50,49 @@ func http_server() {
 	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
 		// Either pick apart the url, or set up a few different
 		// handlers.
-		fmt.Fprintf(w, "play request %s\n", r.URL.Path)
+		log.Printf("play request %s\n", r.URL.Path)
 		values := r.URL.Query()
-		fmt.Fprintf(w, "%d values %s\n", len(values), values)
+		log.Printf("%d values %s\n", len(values), values)
 		if len(values) == 0 {
-			fmt.Fprintf(w, "live stream!\n")
-			live_m3u8(w, r)
+			log.Printf("live stream!\n")
+			live_meta(w, r)
 		}
 
 	})
+
+	http.HandleFunc("/live_index.m3u8", func(w http.ResponseWriter, r *http.Request) {
+		live_index(w, r)
+	})
+
+        http.HandleFunc("/ts/", func(w http.ResponseWriter, r *http.Request) {
+		p := filepath.Clean(r.URL.Path)
+		p = p[len("/ts/"):]
+		log.Printf("ts subpath handler %s\n", p)
+		f, err := os.Open("ts/" + p)
+		if err != nil {
+			log.Printf("%v\n", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "video/MP2T")
+		block := make([]byte, 32000)
+		for {
+			n, _ := f.Read(block)
+			// log.Printf("%d bytes\n", n)
+			if (n == 0) {
+				break
+			}
+			w.Write(block[:n])
+		}
+	})
+
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<html>")
+		fmt.Fprintf(w, `<meta name="viewport" content="width=device-width, initial-scale=1.0">`)
+		fmt.Fprintf(w, `<body>live stream<br><video src="/play"><body></html>`)
+	})
+
+
 
 	err := http.ListenAndServe(":8004", nil)
 	log.Printf(err.Error())
